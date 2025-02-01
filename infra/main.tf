a3cf185875214819a9c8d3735b2186a2cd6bdf9f
aboodm7med1995 <aboodm7med1995@gmail.com>
2025-02-01 21:27:01 +0300

    Resolved merge conflicts

--- infra/main.tf
+++ infra/main.tf
@@ -1,33 +1,42 @@
+terraform {
+  required_providers {
+    contabo = {
+      source  = "ContaboOfficial/contabo"
+      version = "0.3.1"  # Use the latest stable version
+    }
+  }
+}
+
 provider "contabo" {
-    username = "root"
-    password = "7V$C4ib_%:kP"
+  client_id     = "INT-13363627"
+  client_secret = "BnImuy7EEPGEw3pcBUC9rnKBi2FeX5k1"
 }
 
 resource "contabo_instance" "app_server" {
-    image_id      = "Ubuntu 20.04" 
-    instance_type = "G-2000"      
+  image_id      = "ubuntu-20.04"  # Replace with actual image ID
+  instance_type = "G-2000"
 
-    tags = {
-        Name = "AppServer"
+  tags = {
+    Name = "AppServer"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y nginx",
+      "sudo systemctl start nginx",
+      "sudo systemctl enable nginx"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "root"
+      private_key = file("~/.ssh/id_rsa")
+      host        = self.public_ip
     }
-
-    provisioner "remote-exec" {
-        inline = [
-            "sudo apt-get update",
-            "sudo apt-get install -y nginx",
-            "sudo systemctl start nginx",
-            "sudo systemctl enable nginx"
-        ]
-
-        connection {
-            type        = "ssh"
-            user        = "root" # Adjust if necessary
-            private_key = file("~/.ssh/id_rsa")
-            host        = self.public_ip
-        }
-    }
+  }
 }
 
 output "instance_ip" {
-    value = contabo_instance.app_server.public_ip
+  value = contabo_instance.app_server.public_ip
 }
